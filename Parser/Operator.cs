20432d16ab7f25c85f020327055658eba578c60b
Temur <dearpery@gmail.com>
2022-10-26 16:04:15 +0300

    Create Tokenizer class with Tokenize method that do tokenize some expression. Create Operatop class

---
+++ Parser/Operator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftCourse.Parser
+{
+    public class Operator : IComparable<Operator>
+    {
+        private delegate double BinaryFunc(double leftOp, double rightOp);
+
+
+        private BinaryFunc BiOperator;
+
+        
+        public string? Name { get; set; }
+        public int Precedence { get; set; }
+        public bool RightAssociative { get; set; }
+
+        /// <summary>
+        /// return IDictionary containing operators
+        /// </summary>
+        /// <returns></returns>
+        public static IDictionary<string, Operator> GetOperators()
+        {
+            IDictionary<string, Operator> operators = new Dictionary<string, Operator>
+            {
+                ["+"] = new Operator { Name = "+", Precedence = 1, BiOperator = ((double x, double y) => x + y) },
+                ["-"] = new Operator { Name = "-", Precedence = 1, BiOperator = ((double x, double y) => x - y) },
+                ["*"] = new Operator { Name = "*", Precedence = 2, BiOperator = ((double x, double y) => x * y) },
+                ["/"] = new Operator { Name = "/", Precedence = 2, BiOperator = ((double x, double y) => x / y) },
+                //["^"] = new Operator { Name = "^", Precedence = 3, RightAssociative = true, function = ((double x, double y) => Math.Pow(x, y))}
+            };
+            return operators;
+        }
+        public int CompareTo(Operator other)
+        {
+            return this.Precedence - other.Precedence;
+        }
+    }
+}
---
+++ Parser/Tokenizer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftCourse.Parser
+{
+    public enum TokenType { Number, Variable, Function, Parenthesis, Operator, Comma, WhiteSpace };
+    public enum FunctionType { Unary, Binary };
+
+    public struct Token
+    {
+        public TokenType Type { get; }
+        public string Value { get; }
+        public override string ToString()
+        {
+            return $"{Type}: {Value}";
+        }
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+        public override bool Equals( object? obj)
+        {
+            var type = obj.ToString().Split(": ")[0];
+            var value = obj.ToString().Split(": ")[1];
+            obj.ToString();
+            if (this.Type.ToString() != type)
+                return false;
+            if (this.Value.ToString() != value)
+                return false;
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public class Tokenizer
+    {
+        private static IDictionary<string, Operator> operators = Operator.GetOperators();
+        private static TokenType DetermineType(char ch)
+        {
+            if (char.IsLetter(ch) && ch != 'x')
+                return TokenType.Variable;
+            if (char.IsDigit(ch) || ch == '^' || ch == 'x')
+                return TokenType.Number;
+            if (char.IsWhiteSpace(ch))
+                return TokenType.WhiteSpace;
+            if (ch == ',')
+                return TokenType.Comma;
+            if (ch == '(' || ch == ')')
+                return TokenType.Parenthesis;
+            if (operators.ContainsKey(Convert.ToString(ch)))
+                return TokenType.Operator;
+
+            throw new Exception("Wrong character");
+        }
+
+        public static IEnumerable<Token> Tokenize(TextReader reader)
+        {
+            var token = new StringBuilder();
+
+            int curr;
+            while ((curr = reader.Read()) != -1)
+            {
+                var ch = (char)curr;
+                var currType = DetermineType(ch);
+                if (currType == TokenType.WhiteSpace)
+                    continue;
+
+                token.Append(ch);
+
+                var next = reader.Peek();
+                var nextType = next != -1 ? DetermineType((char)next) : TokenType.WhiteSpace;
+                if (currType != nextType)
+                {
+                    if (next == '(')
+                        yield return new Token(TokenType.Function, token.ToString());
+                    else
+                        yield return new Token(currType, token.ToString());
+                    token.Clear();
+                }
+            }
+
+        }
+    }
+}
